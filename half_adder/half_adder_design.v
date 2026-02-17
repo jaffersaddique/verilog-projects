a4739f2b3fd662876578ef2501b42eecfefbc26c
jaffersaddique <jaffersaddique2007@gmail.com>
2026-02-17 19:55:06 +0530

    half_adder using verilog
    half-adder using AMD vivado

---
+++ half_adder/half_adder_design.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.02.2026 20:38:50
+// Design Name: 
+// Module Name: half_adder_design
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module half_adder(
+    input a,
+    input b,
+    output sum,
+    output carry
+    );
+    assign sum=a^b;
+    assign carry=a&b;
+endmodule
